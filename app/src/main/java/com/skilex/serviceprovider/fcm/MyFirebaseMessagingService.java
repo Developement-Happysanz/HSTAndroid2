30d474f6154be5490c52b903c262d5cfa728dcfa
Naren-HappySanz <naren@happysanztech.com>
2019-11-09 17:52:17 +0530

    LAteset

--- app/src/main/java/com/skilex/serviceprovider/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/skilex/serviceprovider/fcm/MyFirebaseMessagingService.java
@@ -8,9 +8,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
